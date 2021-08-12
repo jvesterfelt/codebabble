cb5e10d2e3fdca6ee152a559dc8d28d6f2c735c5
Jamie Vesterfelt <jamievesterfelt@hotmail.com>
2021-08-12 16:38:09 -0600

    Added some additional design features and functionality.

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,15 +4,40 @@ const { User, Blogpost, Comment } = require('../models');
 
 router.get('/', (req, res) => {
     if (req.session.loggedIn) {
-        res.redirect('/dashboard');
+        res.redirect('/');
     }
     res.render('homepage', {
-            loggedIn: req.session.loggedIn
+        loggedIn: req.session.loggedIn
+    })
+});
+
+router.get('/', (req, res) => {
+    Blogpost.findAll({
+            attributes: [
+                'title',
+                'post',
+                'user_id',
+                'created_at'
+            ],
+            include: [{
+                model: Comment,
+                attributes: ['comment_text', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            }]
         })
-        // .catch(err => {
-        //     console.log(err);
-        //     res.status(500).json(err);
-        // });
+        .then(dbBlogpostData => {
+            const posts = dbBlogpostData.map(post => post.get({ plain: true }));
+            console.log(posts)
+            const loggedInUser = { user_id: req.session.user_id }
+            res.render('homepage', { layout: "homepage", posts })
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 router.get('/login', (req, res) => {
@@ -24,10 +49,6 @@ router.get('/login', (req, res) => {
 
 router.get('/signup', (req, res) => {
     res.render('signup', { layout: "blank" })
-        // .catch(err => {
-        //     console.log(err);
-        //     res.status(500).json(err);
-        // });
 });
 
 module.exports = router;
